eec654af32173fad5d2ec3cff2da49c32358d924
Mahir <mahir.cumaliyev12@gmail.com>
2024-12-07 13:15:41 +0400

    Task is done

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Class_work
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+            Console.WriteLine("Enter bullet capacity: ");
+            int capacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter current bullet:");
+            int current = Convert.ToInt32(Console.ReadLine());
+            Weapon weapon1 = new Weapon(capacity, current);
+
+            //0 - İnformasiya almaq üçün
+            //1 - Shoot metodu üçün
+            //2 - Fire metodu üçün
+            //3 - GetRemainBulletCount metodu üçün
+            //4 - Reload metodu üçün
+            //5 - ChangeFireMode metodu üçün
+            //6 - Proqramdan dayandırmaq üçün
+            //qısayoldur.
+
+            bool menu = true;
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("0 - Info\r\n1 - Shoot\r\n2 - Fire\r\n3 - Remain bullet count\r\n4 - Reload\r\n5 - Change fire mode\r\n6 - Exit\r\n7 - Edit");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        Console.Clear();
+                        Console.WriteLine(weapon1);
+                        break;
+                    case "1":
+                        Console.Clear();
+                        weapon1.Shoot();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        weapon1.Fire();
+                        break;
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine(weapon1.GetRemainBulletCount());
+                        break;
+                    case "4":
+                        Console.Clear();
+                        weapon1.Reload();
+                        break;
+                    case "5":
+                        Console.Clear();
+                        weapon1.ChangeFireMode();
+                        break;
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Exit");
+                        menu = false;
+                        break;
+                    //   7 - Edit :
+                    //1 - Tutumu dəyişsin evvelden 30 idise indi 40 olsun
+                    //2 - Güllə sayı deyissin
+                    case "7":
+                        Console.Clear();
+                        Console.WriteLine("1 - Change capacity\r\n2 - Change bullet amount");
+                        switch (Console.ReadLine())
+                        {
+                            case "1":
+                                Console.WriteLine("Enter new capacity:");
+                                weapon1.BulletCapacity = Convert.ToInt32(Console.ReadLine());
+                                if (weapon1.BulletCapacity < weapon1.CurrentBullet) weapon1.CurrentBullet = weapon1.BulletCapacity;
+                                break;
+                            case "2":
+                                Console.WriteLine("Enter bullet amount");
+                                weapon1.CurrentBullet = Convert.ToInt32(Console.ReadLine());
+                                break;
+                            default:
+                                Console.WriteLine("Invalid input");
+                                break;
+                        }
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            } while (menu);
+
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_work
+{
+    //    Weapon deyə class yaradılır.
+    //Darağın güllə tutumu, Daraqdakı güllə sayı,atış modu(təkli, üçlü(burst) və ya avtomatik) propertileri verilir.
+
+    //Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+    public class Weapon
+    {
+        public int BulletCapacity { get; set; }
+
+        private int _currentBullet;
+        public int CurrentBullet
+        {
+            get => _currentBullet;
+            set
+            {
+                if (value <= BulletCapacity) _currentBullet = value;
+                else
+                {
+                    _currentBullet = BulletCapacity;
+                    Console.WriteLine($"Max bullet amount must be {BulletCapacity}.\r\nBullet amount setted to {BulletCapacity}");
+                }
+            }
+        }
+        public Mode WeaponMode { get; set; }
+
+        public Weapon(int capacity, int current)
+        {
+            BulletCapacity = capacity;
+            CurrentBullet = current;
+            ChangeFireMode();
+        }
+
+        //Shoot() - metodu hər çağırıldıqda 1 güllə atır(Ekranda bunu bildirir)
+        public void Shoot()
+        {
+            if (CurrentBullet != 0)
+            {
+                CurrentBullet -= 1;
+                Console.WriteLine("Shoot");
+            }
+            else Console.WriteLine("Empty weapon");
+        }
+        //Fire() - metodu daraqdakı güllələr hamısı atəşlənir. (mod üçlüdürsə(burst) 3 güllə atacaq)
+        public void Fire()
+        {
+            if (CurrentBullet != 0)
+            {
+                if (WeaponMode == Mode.Automatic)
+                {
+                    Console.WriteLine($"Fire {CurrentBullet} times");
+                    CurrentBullet = 0;
+                }
+                else if (WeaponMode == Mode.Burst)
+                {
+                    if (CurrentBullet >= 3)
+                    {
+                        Console.WriteLine("Fire 3 times");
+                        CurrentBullet -= 3;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Fire {CurrentBullet} times");
+                        CurrentBullet = 0;
+                    }
+
+                }
+                else Shoot();
+
+            }
+            else Console.WriteLine("Empty weapon");
+        }
+
+        //GetRemainBulletCount() - darağın tam dolması üçün lazım olan güllə sayını qaytarır.
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - CurrentBullet;
+        }
+        //Reload() - darağı yenidən doldurur.
+        public void Reload()
+        {
+            CurrentBullet = BulletCapacity;
+        }
+        //ChangeFireMode() - Atış modunu dəyişir.
+        public void ChangeFireMode()
+        {
+            Console.WriteLine("Select weapon mode:");
+            Console.WriteLine("1 - Single\r\n2 - Burst\r\n3 - Automatic");
+            int mode = Convert.ToInt32(Console.ReadLine());
+            WeaponMode = (Mode)mode - 1;
+        }
+
+        public override string ToString()
+        {
+            return $"Capacity - {BulletCapacity}\r\nCurrent bullet - {CurrentBullet}\r\nMode - {WeaponMode}";
+        }
+
+    }
+}
